666b52a0c2d76380627af5739447d081ee3abbe2
K.NARENDRA <100072197+NARENDRA-642@users.noreply.github.com>
2024-11-04 11:04:04 +0530

    add files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull narendra642/first-py-cicd-app:latest
+docker pull narendra642/first-py-cicd-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 narendra642/first-py-cicd-app:latest
+docker run -d -p 5000:5000 narendra642/first-py-cicd-app
